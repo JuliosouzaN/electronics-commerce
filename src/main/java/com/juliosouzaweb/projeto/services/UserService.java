f6b93ccdbb38adb7e373b70f9b2e8314ffdd988b
Julio souza <julio_1997_@hotmail.com>
2024-10-28 01:29:38 -0300

    Service layer, component registration

---
+++ src/main/java/com/juliosouzaweb/projeto/services/UserService.java
@@ -0,0 +1,26 @@
+package com.juliosouzaweb.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juliosouzaweb.projeto.entities.User;
+import com.juliosouzaweb.projeto.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
