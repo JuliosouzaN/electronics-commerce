41df0e8a6a86682eb93d1b34319278507f2d39c4
Julio souza <julio_1997_@hotmail.com>
2024-10-28 11:15:19 -0300

    OrderStatus enum

--- src/main/java/com/juliosouzaweb/projeto/entities/Order.java
+++ src/main/java/com/juliosouzaweb/projeto/entities/Order.java
@@ -3,6 +3,8 @@ package com.juliosouzaweb.projeto.entities;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.juliosouzaweb.projeto.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,16 +22,19 @@ public class Order {
 	private Long id;
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
 	public Order() {
 	}
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -49,6 +54,15 @@ public class Order {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();	
+		}
+		
+	}
 	public User getClient() {
 		return client;
 	}
