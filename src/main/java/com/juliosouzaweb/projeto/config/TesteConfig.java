1cbaa8f89094f832b3bdc387494e188fd9cf8287
Julio souza <julio_1997_@hotmail.com>
2024-10-29 22:04:36 -0300

    Payment, one-to-one association

--- src/main/java/com/juliosouzaweb/projeto/config/TesteConfig.java
+++ src/main/java/com/juliosouzaweb/projeto/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.juliosouzaweb.projeto.entities.Category;
 import com.juliosouzaweb.projeto.entities.Order;
 import com.juliosouzaweb.projeto.entities.OrderItem;
+import com.juliosouzaweb.projeto.entities.Payment;
 import com.juliosouzaweb.projeto.entities.Product;
 import com.juliosouzaweb.projeto.entities.User;
 import com.juliosouzaweb.projeto.entities.enums.OrderStatus;
@@ -81,6 +82,9 @@ public class TesteConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
-
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
