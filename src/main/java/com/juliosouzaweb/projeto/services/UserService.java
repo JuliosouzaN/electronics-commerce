9011cb37b233b1b5425c51afe7c0d546b34de4ee
Julio souza <julio_1997_@hotmail.com>
2024-10-30 13:44:49 -0300

    Exception handling - delete

--- src/main/java/com/juliosouzaweb/projeto/services/UserService.java
+++ src/main/java/com/juliosouzaweb/projeto/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.juliosouzaweb.projeto.entities.User;
 import com.juliosouzaweb.projeto.repositories.UserRepository;
+import com.juliosouzaweb.projeto.services.exceptions.DatabaseException;
 import com.juliosouzaweb.projeto.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,13 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
